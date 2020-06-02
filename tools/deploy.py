8d9eeb81baf52f13eb741021c4aee698994e83c5
tanshihaj <kamil@zaripov.net>
2020-06-03 01:45:32 +0300

    test sftp deployer

--- tools/deploy.py
+++ tools/deploy.py
@@ -1,21 +1,27 @@
 import os
 import subprocess
+import pysftp
 
 assert 'DEPLOY_HOST' in os.environ
 assert 'DEPLOY_HOST_PATH' in os.environ
-assert 'DEPLOY_HOST_SSH_USER' in os.environ
-assert 'DEPLOY_HOST_SSH_PASSWORD' in os.environ
+assert 'DEPLOY_HOST_SFTP_USER' in os.environ
+assert 'DEPLOY_HOST_SFTP_PASSWORD' in os.environ
 assert 'TRAVIS_BUILD_DIR' in os.environ
 
-src_file = os.path.join(os.environ['TRAVIS_BUILD_DIR'], 'data_model.py')
-dst_file =         os.environ['DEPLOY_HOST_SSH_USER'] \
-           + ':' + os.environ['DEPLOY_HOST_SSH_PASSWORD'] \
-           + '@' + os.environ['DEPLOY_HOST'] \
-           + ':' + os.path.join(os.environ['TRAVIS_BUILD_DIR'], 'data_model.py')
-proc = subprocess.Popen(["scp", src_file, dst_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = proc.communicate(timeout=15)
+with pysftp.Connection(os.environ['DEPLOY_HOST'], username=os.environ['DEPLOY_HOST_SFTP_USER'], password=os.environ['DEPLOY_HOST_SFTP_PASSWORD']) as sftp:
+    src_dir = os.environ['TRAVIS_BUILD_DIR']
+    dst_dir = os.environ['DEPLOY_HOST_PATH']
+    if not sftp.exists(dst_dir):
+        sftp.mkdir(dst_dir)
+    with sftp.cd(dst_dir):
+        # clean old code
+        for item in sftp.listdir():
+            if item == 'logs' or item == 'secrets.json':
+                continue
+            sftp.remove(item)
 
-print('scp retcode %s' % proc.returncode)
-print('scp stdout "%s"' % stdout)
-print('scp stderr "%s"' % stderr)
-
+        # upload new code
+        sftp.put(src_dir + '/data_model.py')
+        sftp.put(src_dir + '/passenger_wsgi.py')
+        sftp.put(src_dir + '/request_handler.py')
+        sftp.put(src_dir + '/settings.py')
