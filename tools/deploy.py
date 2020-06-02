987369e5d4d97b99677c750aeeaeecb64aec1e61
tanshihaj <kamil@zaripov.net>
2020-06-03 01:53:11 +0300

    remove host key check for deploy.py

--- tools/deploy.py
+++ tools/deploy.py
@@ -8,7 +8,13 @@ assert 'DEPLOY_HOST_SFTP_USER' in os.environ
 assert 'DEPLOY_HOST_SFTP_PASSWORD' in os.environ
 assert 'TRAVIS_BUILD_DIR' in os.environ
 
-with pysftp.Connection(os.environ['DEPLOY_HOST'], username=os.environ['DEPLOY_HOST_SFTP_USER'], password=os.environ['DEPLOY_HOST_SFTP_PASSWORD']) as sftp:
+host = os.environ['DEPLOY_HOST']
+username = os.environ['DEPLOY_HOST_SFTP_USER']
+password = os.environ['DEPLOY_HOST_SFTP_PASSWORD']
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
+
+with pysftp.Connection(host, username=username, password=password, cnopts=cnopts) as sftp:
     src_dir = os.environ['TRAVIS_BUILD_DIR']
     dst_dir = os.environ['DEPLOY_HOST_PATH']
     if not sftp.exists(dst_dir):
@@ -25,3 +31,5 @@ with pysftp.Connection(os.environ['DEPLOY_HOST'], username=os.environ['DEPLOY_HO
         sftp.put(src_dir + '/passenger_wsgi.py')
         sftp.put(src_dir + '/request_handler.py')
         sftp.put(src_dir + '/settings.py')
+
+print('Deploy finished successfully')
